59e1ec373d9d88f642dfe86cab226260d61f12be
ThomasAnt <thomas.antensteiner@hotmail.com>
2019-10-14 17:35:37 +0200

    Lesson:1-18 Thomas Antensteiner

--- app/src/main/java/at/htl/diceroller/MainActivity.kt
+++ app/src/main/java/at/htl/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package at.htl.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +16,15 @@ class MainActivity : AppCompatActivity() {
         val rollButton:Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's roll!";
         rollButton.setOnClickListener{
-            Toast.makeText(this,"button_clicked",Toast.LENGTH_SHORT).show()
+            rollDice();
         }
     }
+
+    private fun rollDice() {
+        val resultText:TextView = findViewById(R.id.result_text)
+        val random: Int = java.util.Random().nextInt(6) +1
+        resultText.text = random.toString()
+    }
+
+
 }
