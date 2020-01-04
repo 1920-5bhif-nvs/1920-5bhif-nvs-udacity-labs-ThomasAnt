e73eb5c934180439a66a4cec815d432cd5377043
ThomasAnt <thomas.antensteiner@hotmail.com>
2020-01-04 11:28:10 +0100

    Lesson_3-14 Thomas Antensteiner

--- Lesson_3/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ Lesson_3/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -23,6 +23,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 
@@ -33,6 +34,7 @@ class GameWonFragment : Fragment() {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
         binding.nextMatchButton.setOnClickListener( {view:View -> view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)})
+
         return binding.root
     }
 }
