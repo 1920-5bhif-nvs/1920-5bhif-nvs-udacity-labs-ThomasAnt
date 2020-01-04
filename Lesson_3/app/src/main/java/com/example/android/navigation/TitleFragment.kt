69f609e8e635b8e74e54da4ec55702cefd9f35fc
ThomasAnt <thomas.antensteiner@hotmail.com>
2020-01-04 10:48:02 +0100

    Lesson_3-9 Thomas Antensteiner

--- Lesson_3/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Lesson_3/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -15,6 +16,10 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding:FragmentTitleBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false);
+
+        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        //binding.playButton.setOnClickListener({view:View -> Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)})
+
         return binding.root;
     }
 }
