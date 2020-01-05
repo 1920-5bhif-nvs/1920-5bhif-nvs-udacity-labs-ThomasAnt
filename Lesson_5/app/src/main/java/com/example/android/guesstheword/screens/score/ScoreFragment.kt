a324910163ca1829917326358d406fb82c889bd0
ThomasAnt <thomas.antensteiner@hotmail.com>
2020-01-05 21:07:17 +0100

    Lesson_5-22 Thomas Antensteiner

--- Lesson_5/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ Lesson_5/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
@@ -31,6 +34,9 @@ import com.example.android.guesstheword.databinding.ScoreFragmentBinding
  * Fragment where the final score is shown, after the game is over
  */
 class ScoreFragment : Fragment() {
+
+    private lateinit var viewModel: ScoreViewModel
+    private lateinit var viewModelFactory: ScoreViewModelFactory
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -46,11 +52,22 @@ class ScoreFragment : Fragment() {
                 false
         )
 
+
+
+        binding.playAgainButton.setOnClickListener { viewModel.playAgain() }
+
         // Get args using by navArgs property delegate
         val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
-        binding.scoreText.text = scoreFragmentArgs.score.toString()
-        binding.playAgainButton.setOnClickListener { onPlayAgain() }
+        viewModelFactory = ScoreViewModelFactory(scoreFragmentArgs.score)
+        viewModel = ViewModelProviders.of(this,viewModelFactory).get(ScoreViewModel::class.java)
 
+        viewModel.finalScore.observe(this, Observer { newFinalScore -> binding.scoreText.text = newFinalScore.toString() })
+        viewModel.eventPlayAgain.observe(this, Observer { playAgain ->
+            if(playAgain){
+                onPlayAgain()
+                viewModel.onPlayAgainComplete()
+            }
+        })
         return binding.root
     }
 
