bc9f4981e78af9e5a6151e302d826bdd4c215805
ThomasAnt <thomas.antensteiner@hotmail.com>
2020-01-06 13:53:17 +0100

    Lesson_7-10 Thomas Antensteiner

--- Lesson_7/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ Lesson_7/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -43,15 +43,21 @@ class SleepNightAdapter:RecyclerView.Adapter<SleepNightAdapter.ViewHolder>(){
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        var view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_sleep_night,parent,false)
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
 
-    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+    class ViewHolder private constructor(itemView: View):RecyclerView.ViewHolder(itemView){
         val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
         val quality: TextView = itemView.findViewById(R.id.quality_string)
         val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                var view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_sleep_night, parent, false)
+                return ViewHolder(view)
+            }
+        }
 
         fun bind(item: SleepNight) {
             val res = itemView.context.resources
@@ -68,4 +74,6 @@ class SleepNightAdapter:RecyclerView.Adapter<SleepNightAdapter.ViewHolder>(){
             })
         }
     }
+
+
 }
